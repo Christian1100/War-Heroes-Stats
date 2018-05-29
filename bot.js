d108639273d998fc324a21d723e0da6dd61e95d5
Christian1100 <39745022+Christian1100@users.noreply.github.com>
2018-05-29 23:54:06 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,22 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+client.login(process.env.BOT_TOKEN);
+
 client.on('ready', () => {
     console.log('I am ready!');
 });
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong!');
-  	}
+client.on('message', msg => {
+    var cont = msg.content;
+    author = msg.member;
+    chan = msg.channel;
+    guild = msg.guild;
+    
+    if (author.id != client.user.id && cont.startsWith(';'))
+    {
+        msg.channel.send('pong');
+    }
 });
 
-// THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);
+
