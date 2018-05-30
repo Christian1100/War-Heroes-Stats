94a597f30d30f624a38ed081d921233e84fb082f
Christian1100 <39745022+Christian1100@users.noreply.github.com>
2018-05-30 20:38:30 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,24 +1,16 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
 
 client.login(process.env.BOT_TOKEN);
 
-client.on('ready', () => {
-    console.log('I am ready!');
+client.on("ready", async() => {
+    console.log("Bot is online!");
 });
 
-client.on('message', msg => {
-    var cont = msg.content;
-    author = msg.member;
-    chan = msg.channel;
-    guild = msg.guild;
-    
-    if (author.id != client.user.id && cont.startsWith(';'))
-    {
-        if(cont.startsWith(';ping')) 
-        {
-            msg.channel.send(new Date().getTime() - msg.createdTimestamp + ' ms');        
-        }
-    }
+let prefix = ";";
+client.on("message", async(message) => {
+	if (message.content.startsWith(`${prefix}ping`)) {
+		await message.channel.send(new Date().getTime() - msg.createdTimestamp + " ms");
+	}
 });
 
